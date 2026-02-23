198f43e2051428df9b1bd20c1959b687fc5afd03
Henrique <henriqueabatista09@gmail.com>
2026-02-23 08:47:10 -0300

    Add files via upload

---
+++ aula1/js/variavel.js
@@ -0,0 +1,22 @@
+var nomeDaVariavel = "valor da váriavel";
+var outraVariavel = "valor da outra váriavel";
+var variavelIdade = 22;
+var variavelTenhoCabeloBranco = false;
+var variavelIndefinida ;
+
+console.log(nomeDaVariavel);
+console.log(outraVariavel);
+console.log(variavelIdade);
+console.log(variavelTenhoCabeloBranco);
+console.log(variavelIndefinida);
+
+
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+console.log(tr);
+console.log(fl);
