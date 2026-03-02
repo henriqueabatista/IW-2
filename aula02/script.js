10170d6f715014869a625fa91edeb01e40004256
Henrique <henriqueabatista09@gmail.com>
2026-03-02 08:07:24 -0300

    aula 2

---
+++ aula02/script.js
@@ -0,0 +1,40 @@
+console.log("Exercicio A:");
+var a = 10;
+var b = 20;
+console.log( b );
+b = 5;
+console.log (a , b);
+
+console.log("Exercicio B:");
+a = 30;
+b = 20;
+var c = a + b;
+console.log(c);
+b = 10;
+console.log(b,c);
+c = a + b;
+console.log(a,b,c);
+
+console.log("Exercicio C:");
+a = 10;
+b = 20;
+c = a;
+b = c;
+a = b;
+console.log(a,b,c) 
+
+console.log("Exercicio D:");
+a = 10;
+b = a + 1;
+a = b + 1;
+b = a + 1;
+console.log(a);
+a = b + 1;
+console.log(a,b);
+
+console.log("exercicio 2:");
+var n1 = 2;
+var n2 = 3;
+var n3 = 5;
+var mediaFinal = (n1*2 + n2*3 + n3*5)/10
+console.log(mediaFinal)
